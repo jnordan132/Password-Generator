cb4d6d3a419eac4d15279784295631eae7dceae0
jacob nordan <jacob.nordan113322@gmail.com>
2022-02-12 13:01:27 -0800

    previous project push to work off of

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import WritePassword from './components/GeneratorCard';
+import PasswordInfo from './components/PasswordInfo';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+      <br />
+      <PasswordInfo />
+      </div>
+    </div>
+  );
+}
+
+export default App;
